60d098c121ee65e10a48dbb27f7fb135b34ebd1f
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-08-09 19:56:44 +0200

    :notebook:  Add Canvas and Graphics
    Importing svg code into swift to use it as a shape

---
+++ IOS15App/HexagonView.swift
@@ -0,0 +1,58 @@
+//
+//  HexagonView.swift
+//  IOS15App
+//
+//  Created by Pat on 2022/08/09.
+//
+
+//MARK: - Canvas
+///This allows us to draw compose and animate mutiple elements
+///this gives us a perfomance gain that will give us more control
+///Importing svg code into swift to use it as a shape
+import SwiftUI
+
+struct HexagonView: View {
+    var body: some View {
+        Canvas{ context , size in
+            context.fill(path(in: CGRect(x: 0, y: 0, width: size.width, height: size.height)), with:
+                    .linearGradient(Gradient(colors: [.red,.purple]), startPoint: CGPoint(x: 0, y: 0), endPoint: CGPoint(x: 300, y: 100))
+            )
+        }
+        .frame(width: 400, height: 414)
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let width = rect.size.width
+        let height = rect.size.height
+        path.move(to: CGPoint(x: 0.9923*width, y: 0.42593*height))
+        path.addCurve(to: CGPoint(x: 0.6355*width, y: height), control1: CGPoint(x: 0.92554*width, y: 0.77749*height), control2: CGPoint(x: 0.91864*width, y: height))
+        path.addCurve(to: CGPoint(x: 0.08995*width, y: 0.60171*height), control1: CGPoint(x: 0.35237*width, y: height), control2: CGPoint(x: 0.2695*width, y: 0.77304*height))
+        path.addCurve(to: CGPoint(x: 0.34086*width, y: 0.06324*height), control1: CGPoint(x: -0.0896*width, y: 0.43038*height), control2: CGPoint(x: 0.00248*width, y: 0.23012*height))
+        path.addCurve(to: CGPoint(x: 0.9923*width, y: 0.42593*height), control1: CGPoint(x: 0.67924*width, y: -0.10364*height), control2: CGPoint(x: 1.05906*width, y: 0.07436*height))
+        path.closeSubpath()
+        return path
+    }
+}
+
+struct HexagonView_Previews: PreviewProvider {
+    static var previews: some View {
+        HexagonView()
+    }
+}
+
+
+struct MyCustomBlob: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let width = rect.size.width
+        let height = rect.size.height
+        path.move(to: CGPoint(x: 0.9923*width, y: 0.42593*height))
+        path.addCurve(to: CGPoint(x: 0.6355*width, y: height), control1: CGPoint(x: 0.92554*width, y: 0.77749*height), control2: CGPoint(x: 0.91864*width, y: height))
+        path.addCurve(to: CGPoint(x: 0.08995*width, y: 0.60171*height), control1: CGPoint(x: 0.35237*width, y: height), control2: CGPoint(x: 0.2695*width, y: 0.77304*height))
+        path.addCurve(to: CGPoint(x: 0.34086*width, y: 0.06324*height), control1: CGPoint(x: -0.0896*width, y: 0.43038*height), control2: CGPoint(x: 0.00248*width, y: 0.23012*height))
+        path.addCurve(to: CGPoint(x: 0.9923*width, y: 0.42593*height), control1: CGPoint(x: 0.67924*width, y: -0.10364*height), control2: CGPoint(x: 1.05906*width, y: 0.07436*height))
+        path.closeSubpath()
+        return path
+    }
+}
