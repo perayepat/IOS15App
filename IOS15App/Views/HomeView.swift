2333e63bfb81c09c84896233552f815406422f39
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-08-21 09:02:12 +0200

    :notebook: Add Parallax and 3D rotation effect
    Using the geometry reader and the blur effect for the swipe gestures

--- IOS15App/Views/HomeView.swift
+++ IOS15App/Views/HomeView.swift
@@ -57,8 +57,27 @@ struct HomeView: View {
     var featured: some View{
         //MARK: - Custom Horizontal Scroll
         TabView {
-            ForEach(courses) { item in
-                FeaturedItem(course: item)
+            ForEach(courses) { course in
+                GeometryReader { proxy in
+                    let minX  = proxy.frame(in: .global).minX
+                    
+                    FeaturedItem(course: course)
+                        .padding(.vertical, 40)
+                        .rotation3DEffect(.degrees( minX / -10), axis: (x: 0, y: 1, z: 0))
+                        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+                        .blur(radius: abs(minX / 40))
+                    
+                    //MARK: - Parallax effect
+                        .overlay(
+                            Image(course.image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 230)
+                                .offset(x: 32, y: -80)
+                                .offset(x: minX / 2)
+                                .blur(radius: abs(minX / 80))
+                    )
+                }
             }
         }
         .tabViewStyle(.page(indexDisplayMode: .never))
